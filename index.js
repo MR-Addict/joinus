4b73cb843e8bc1c7796c2908292b0bd5eabe9f12
MR-Addict <2750417853@qq.com>
2022-08-22 15:41:41 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Offical packages
+const express = require("express");
+
+// Offical variable
+const app = express();
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// Offical middleware
+app.use(express.static("public"));
+
+// Router
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+// Listening on port 8084
+const port = process.env.PORT || 8084;
+app.listen(port, () => console.log(`Listening on port http://localhost:${port}...`));
