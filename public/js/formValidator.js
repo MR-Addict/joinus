8ece1047c3682375861c3ab38cf59bd12a846593
MR-Addict <2750417853@qq.com>
2022-08-23 12:55:32 +0800

    update:replace default validation message with custom validation

---
+++ public/js/formValidator.js
@@ -0,0 +1,59 @@
+export default class formValidator {
+  constructor(selector) {
+    this.form = document.querySelector(selector);
+    this.inputsWithErrors = new Set();
+
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (!this.hasErrors) {
+        this.form.submit();
+      }
+    });
+  }
+
+  get hasErrors() {
+    return this.inputsWithErrors.size > 0;
+  }
+
+  register(selector, check) {
+    const inputField = this.form.querySelector(selector);
+    const errorElement = inputField.closest(".form-element").querySelector(".err-msg");
+
+    const execute = (hideErrors) => {
+      const { pass, error } = check(inputField.value, inputField);
+      if (!hideErrors) errorElement.textContent = error || "";
+      if (pass) {
+        this.inputsWithErrors.delete(inputField);
+      } else {
+        this.inputsWithErrors.add(inputField);
+      }
+    };
+    inputField.addEventListener("change", () => execute());
+    execute(true);
+  }
+  registerForSelects() {
+    const select1 = document.getElementById("第一志愿");
+    const select2 = document.getElementById("第二志愿");
+    const errorElement1 = select1.closest(".form-element").querySelector(".err-msg");
+    const errorElement2 = select2.closest(".form-element").querySelector(".err-msg");
+
+    const execute = (hideErrors) => {
+      if (select1.value === select2.value) {
+        if (!hideErrors) {
+          errorElement1.textContent = "志愿重复，请修改第一或第二志愿";
+          errorElement2.textContent = "志愿重复，请修改第一或第二志愿";
+        }
+        this.inputsWithErrors.add(select1);
+        this.inputsWithErrors.add(select2);
+      } else {
+        errorElement1.textContent = "";
+        errorElement2.textContent = "";
+        this.inputsWithErrors.delete(select1);
+        this.inputsWithErrors.delete(select2);
+      }
+    };
+    select1.addEventListener("change", () => execute());
+    select2.addEventListener("change", () => execute());
+    execute(true);
+  }
+}
---
+++ public/js/index.js
@@ -0,0 +1,159 @@
+import formValidator from "./formValidator.js";
+const fv = new formValidator("#form");
+
+// name validation
+fv.register("#姓名", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "姓名不能为空哦！",
+    };
+  } else if (value.length > 10) {
+    return {
+      pass: false,
+      error: "名字太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// phone validation
+fv.register("#手机", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "手机号不能为空哦！",
+    };
+  } else if (value.length != 11) {
+    return {
+      pass: false,
+      error: "手机号长度错误！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// QQ validation
+fv.register("#QQ", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "QQ号不能为空哦！",
+    };
+  } else if (value.length > 11 || value.length < 5) {
+    return {
+      pass: false,
+      error: "QQ号长度错误！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// email validation
+fv.register("#邮箱", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "邮箱不能为空哦！",
+    };
+  } else if (!value.includes("@")) {
+    return {
+      pass: false,
+      error: "邮箱格式错误",
+    };
+  } else if (value.length > 30) {
+    return {
+      pass: false,
+      error: "邮箱太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// id validation
+fv.register("#学号", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "学号不能为空哦！",
+    };
+  } else if (value.length != 12) {
+    return {
+      pass: false,
+      error: "学号长度错误！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// academy validation
+fv.register("#学院", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "学院不能为空哦！",
+    };
+  } else if (value.length > 30) {
+    return {
+      pass: false,
+      error: "学院名字太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// profession validation
+fv.register("#专业", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "专业不能为空哦！",
+    };
+  } else if (value.length > 30) {
+    return {
+      pass: false,
+      error: "专业名字太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// introduce validation
+fv.register("#自我介绍", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "自我介绍不能为空哦！",
+    };
+  } else if (value.length < 4) {
+    return {
+      pass: false,
+      error: "字数太少啦，至少4个字哦！",
+    };
+  } else if (value.length > 500) {
+    return {
+      pass: false,
+      error: "自我介绍太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// 第一志愿
+fv.registerForSelects();
