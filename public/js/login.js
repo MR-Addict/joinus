6d2902ca58ff24191925336d59071dae84175bbd
MR-Addict <2750417853@qq.com>
2022-08-24 00:18:51 +0800

    add:add validation for login

---
+++ public/js/login.js
@@ -0,0 +1,38 @@
+import formValidator from "./formValidator.js";
+const fv = new formValidator(".form");
+
+// name validation
+fv.register("#username", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "用户名不能为空哦！",
+    };
+  } else if (value.length > 10) {
+    return {
+      pass: false,
+      error: "用户名太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
+
+// password validation
+fv.register("#password", (value, inputField) => {
+  if (value.length === 0) {
+    return {
+      pass: false,
+      error: "密码不能为空哦！",
+    };
+  } else if (value.length > 20) {
+    return {
+      pass: false,
+      error: "密码太长啦！",
+    };
+  }
+  return {
+    pass: true,
+  };
+});
